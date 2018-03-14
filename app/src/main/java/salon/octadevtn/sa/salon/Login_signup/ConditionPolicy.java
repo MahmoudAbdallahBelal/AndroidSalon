71dd95e281d42a215645fa76a21aa975f475c37f
Hend-iti <hendibrahim@hotmail.com>
2018-03-14 20:58:49 +0200

    Commit Changes

--- app/src/main/java/salon/octadevtn/sa/salon/Login_signup/ConditionPolicy.java
+++ app/src/main/java/salon/octadevtn/sa/salon/Login_signup/ConditionPolicy.java
@@ -2,6 +2,7 @@ package salon.octadevtn.sa.salon.Login_signup;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.webkit.WebView;
 import android.widget.LinearLayout;
 
@@ -19,13 +20,24 @@ public class ConditionPolicy extends AppCompatActivity {
         //ButterKnife.bind(this);
 
         webView= (WebView) findViewById(R.id.webview1);
-        //back=findViewById(R.id.bac);
+        back=findViewById(R.id.bac);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
         if (MyApplication.getInstance().getPrefManager().getLang().equals("0"))
         {
             webView.getSettings().setJavaScriptEnabled(true);
             webView.loadUrl("http://salon.com.sa/Terms.htm");
-        } else {
+        }
+        if (MyApplication.getInstance().getPrefManager().getLang().equals("1")) {
             webView.getSettings().setJavaScriptEnabled(true);
             webView.loadUrl("http://salon.com.sa/TermsAr.htm");
         }
@@ -35,8 +47,7 @@ public class ConditionPolicy extends AppCompatActivity {
     public void onBackPressed() {
 
         super.onBackPressed();
-        ConditionPolicy.this.overridePendingTransition(R.anim.slide_in_right,
-                R.anim.slide_out_right);
+        ConditionPolicy.this.overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_right);
         finish();
 
     }
